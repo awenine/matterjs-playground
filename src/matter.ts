bc9b41e8a04255817569cb9fb86ece92717f0782
Chris Owen <c.ornine@gmail.com>
2024-03-18 22:50:35 +0000

    feat: add contraints to scene objects

--- src/matter.ts
+++ src/matter.ts
@@ -1,4 +1,4 @@
-import { Engine, Bodies, Render, Composite, Runner } from 'matter-js';
+import { Engine, Bodies, Render, Composite, Runner, Constraint } from 'matter-js';
 
 // create an engine
 const engine = Engine.create();
@@ -11,16 +11,43 @@ const render = Render.create({
 });
 
 // create two boxes and a ground
-const boxA = Bodies.rectangle(400, 200, 80, 80);
-const boxB = Bodies.rectangle(450, 50, 80, 80);
-const otherBody = Bodies.circle(500,300,70)
+// const boxA = Bodies.rectangle(400, 200, 80, 80);
+// const boxB = Bodies.rectangle(450, 50, 80, 80);
+const otherBody = Bodies.circle(400,300,50, {isStatic:true})
 const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
 
+const chainLinks = []
+const chainConstraints = []
+for (let i = 0; i < 12; i++) {
+  chainLinks.push(Bodies.circle(((i+1)*50)+30,50,20))
+  if (i>0 ){
+chainConstraints.push(Constraint.create({
+  bodyA: chainLinks[i-1],
+  bodyB: chainLinks[i],
+  stiffness:0.4,
+    render:{
+    visible: false
+  }
+}))
+  }
+  
+}
 
+chainLinks[0].isStatic = true
+
+
+// const boxSpring = Constraint.create({
+//   bodyA:boxA,
+//   bodyB:boxB,
+//   stiffness: 0.1,
+//   render:{
+//     visible: false
+//   }
+// })
 
 // add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB,otherBody, ground]);
+Composite.add(engine.world, [ ground, ...chainLinks, ...chainConstraints, otherBody]);
 
 // run the renderer
 Render.run(render);
