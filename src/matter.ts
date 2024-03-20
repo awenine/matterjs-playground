c3495858b67c95f3321a6d986fa129e4218c57b4
Chris Owen <c.ornine@gmail.com>
2024-03-20 23:54:16 +0000

    feat: add automatic move body to point function

--- src/matter.ts
+++ src/matter.ts
@@ -1,4 +1,4 @@
-import { Engine, Bodies, Render, Composite, Runner, Constraint, MouseConstraint, Mouse, Body, Vector, Events } from 'matter-js';
+import { Engine, Bodies, Render, Composite, Runner, Constraint, MouseConstraint, Mouse, Body, Vector, Events, Sleeping } from 'matter-js';
 
 import chrisMusicPng from '/chris-music.png'
 
@@ -43,9 +43,19 @@ const boxA = Bodies.rectangle(400, 200, 80, 80,
     }
   } 
 );
+
+const litterBoxes = [350,450,550].map(x => {
+  return Bodies.rectangle(x, 100, 80, 80,
+    {
+      restitution:0.7,
+    } 
+  );
+})
 // const boxB = Bodies.rectangle(450, 50, 80, 80);
-const otherBody = Bodies.circle(400,300,50, {isStatic:true})
+// const otherBody = Bodies.circle(400,300,50, {isStatic:true})
 const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+const rightWall = Bodies.rectangle(30, 300, 60, 600, { isStatic: true });
+const leftWall = Bodies.rectangle(790, 300, 60, 600, { isStatic: true });
 
 
 
@@ -89,12 +99,7 @@ let flippedColour = '#ff0000'
 
 // });
 
-// Events.on(mouseConstraint, 'enddrag', function(event) {
-//   console.log('enddrag', event.body);
-//   event.body.render.fillStyle = flippedColour
-//   Body.setVelocity(event.body, Vector.create(0,-7))
-//   event.body.circleRadius = 20
-// });
+
 
 let pairCounter = 0  
 
@@ -133,10 +138,72 @@ setTimeout(() => {
 },6000)
 
 
+// move body code - from https://github.com/liabru/matter-js/issues/733
+
+
+//? NOTE - as this method doesnt use velocity, it doesnt move other object out of the way naturally - how to adapt?
+function forceMoveAndRotate(body:Body, endX:number, endY:number, pct:number) {
+  // dx is the total distance to move in the X direction
+  let dx = endX - body.position.x;
+
+  // dy is the total distance to move in the Y direction
+  let dy = endY - body.position.y;
+
+
+  // use dx & dy to calculate where the current [x,y] is at a given pct
+  let x = body.position.x + (dx * pct) / 100;
+  let y = body.position.y + (dy * pct) / 100;
+  let r = (body.angle / 100) * (100 - pct)
+
+  Body.setPosition(body, {
+      x: x,
+      y: y
+  });
+
+  Body.setAngle(body, r)
+}
+
+let pct = 0;
+const targetX = 400
+const targetY = 300
+
+let move = false
+let movableBody = boxA
+
+Events.on(mouseConstraint, 'enddrag', function(event) {
+  console.log('enddrag', event.body);
+  // event.body.render.fillStyle = flippedColour
+  // Body.setVelocity(event.body, Vector.create(0,-7))
+  // event.body.circleRadius = 20
+  // if (event.body.id = 1) {
+    console.log('boxA dragged');
+    if (!event.body.isStatic){
+    Sleeping.set(movableBody, false)
+    pct = 0
+    movableBody = event.body
+    move = true}
+});
+
+
+Events.on(engine, "beforeUpdate", function (_event) {
+    if (move){ 
+      if(pct < 101) {
+        pct = pct + 1;
+        forceMoveAndRotate(movableBody, targetX, targetY, pct);
+      } else {
+        // set angle to upright
+        movableBody.angle = 0
+        Sleeping.set(movableBody, true)
+        move = false
+      }
+  }
+});
+
+
 
 
 // add all of the bodies to the world
-Composite.add(engine.world, [ ground, ...chainLinks, ...chainConstraints, otherBody, mouseConstraint, boxA, ]);
+Composite.add(engine.world, [ ground, rightWall, leftWall, ...chainLinks, ...chainConstraints, mouseConstraint, boxA, ...litterBoxes ]);
 
 // run the renderer
 Render.run(render);
